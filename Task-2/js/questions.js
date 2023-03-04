61a0299ea25c5c0e0d64bffa4c2c8de03401885c
Anisfathima27 <122045826+Anisfathima27@users.noreply.github.com>
2023-03-04 10:48:02 +0530

    Add files via upload

---
+++ Task-2/js/questions.js
@@ -0,0 +1,131 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Applied Programming Interface",
+      "Applicable Programming Interface",
+      "Application Programming Interface",
+      "Application Program Interface"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which type of javascript languge is?",
+    answer: "Object Based",
+    options: [
+      "Obejct oriented",
+      "Object Based",
+      "Assembly Language",
+      "High Level"
+    ]
+  },
+  {
+  numb: 8,
+  question: "In HTML document the current sequence of the tags for starting a webpage is:?",
+  answer: "HTML,head,title,body",
+  options: [
+    "Head,title,HTML,body",
+    "HTML,title,Head,body",
+    "HTML,head,title,body",
+    "HTML,body,title,head"
+  ]
+ },
+ {
+ numb: 9,
+ question: "what is the smallest header in HTML by default?",
+ answer: "h6",
+ options: [
+   "h1",
+   "h2",
+   "h4",
+   "h6"
+ ]
+},
+{
+numb: 10,
+question: "Which event can be generated when a drag-and-drop operation is undertaken?",
+answer: "ondragdrop",
+options: [
+  "ondrop",
+  "ondrag",
+  "ondragdrop",
+  "None of the above"
+]
+},
+
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
